433eb6ad93d126305669cd169765e3c913567a2e
Code Head <codehead@codelegends.com>
2025-03-02 15:14:51 -0500

    enabled s3 bucket

--- medusajs-storefront/next.config.js
+++ medusajs-storefront/next.config.js
@@ -41,6 +41,14 @@ const nextConfig = withStoreConfig({
         protocol: 'https',
         hostname: 'medusa-server-testing.s3.us-east-1.amazonaws.com',
       },
+      {
+        protocol: 'http',
+        hostname: 'yzymedia.s3.us-east-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yzymedia.s3.us-east-1.amazonaws.com',
+      }
     ],
   },
 })
